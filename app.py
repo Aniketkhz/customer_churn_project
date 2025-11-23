63275657c00847dad0e785d96f0ea53c191610f1
Aniketkhz <aniketkhadanga2@gmail.com>
2025-11-23 18:36:37 +0530

    create dataframe

--- app.py
+++ app.py
@@ -142,7 +142,7 @@ def predict():
             except:
                 processed_data['TotalCharges'] = 0
         
-        # Create DataFrame from inp
+        # Create DataFrame from in
         input_df = pd.DataFrame([processed_data])
         
         # One-hot encode categorical features
